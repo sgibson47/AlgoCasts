eba8cb1ec5fb88c66cd288da1a33bb38d1d2666a
sgibson47 <sgibson47@gmail.com>
2022-01-08 15:03:00 -0600

    add soltuions from Gibson & Grider

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,51 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for(let i = 1; i <= n; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log('fizzbuzz')
+        } else if (divByThree = i % 3 === 0){
+            console.log('fizz')
+        } else if (divByThree = i % 5 === 0){
+            console.log('buzz') 
+        } else {
+            console.log(i)
+        }
+    }
+}
 
 module.exports = fizzBuzz;
+
+// Gibson - Solution 1
+// function fizzBuzz(n) {
+//     for(let i = 1; i < n+1; i++){
+//         const divByThree = i % 3 == 0
+//         const divByFive = i % 5 == 0
+        
+//         if(divByThree && !divByFive){
+//             console.log('fizz')
+//         } else if (!divByThree && divByFive){
+//             console.log('buzz')
+//         } else if (divByThree && divByFive){
+//             console.log('fizzbuzz')
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
+
+// Grider - Solution 1
+// function fizzBuzz(n) {
+//     for(let i = 1; i <= n; i++){
+//         if(i % 3 === 0 && i % 5 === 0){
+//             console.log('fizzbuzz')
+//         } else if (divByThree = i % 3 === 0){
+//             console.log('fizz')
+//         } else if (divByThree = i % 5 === 0){
+//             console.log('buzz') 
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
